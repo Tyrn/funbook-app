c67021cdc46166f7aac87b1008a69cc629a71a60
Tyrn <alexey1911@gmail.com>
2024-11-11 18:23:09 +0300

    import React failed

---
+++ src/surfaces/Feed.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+export const Feed = () => {
+  return (
+    <View>
+      <Text>this will be the feed screen</Text>
+    </View>
+  );
+};
