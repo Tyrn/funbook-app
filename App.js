22e6204f1b80ea2f07e46ae0de8110424424d3c3
Tyrn <alexey1911@gmail.com>
2024-11-11 18:04:00 +0300

    Navigation imports to App.js

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import "react-native-gesture-handler";
+//import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
